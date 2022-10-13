ef20b3684beca90491488b2bc08f0a94a387955f
yappipi <yappipi@gmail.com>
2022-10-13 21:58:56 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,143 @@
+import myEpicNft from "./utils/MyEpicNFT.json";
+import { ethers } from 'ethers';
+import React, { useEffect, useState } from 'react';
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
-import React from "react";
 
 // Constantsを宣言する: constとは値書き換えを禁止した変数を宣言する方法です。
-const TWITTER_HANDLE = 'あなたのTwitterのハンドルネームを貼り付けてください';
+const TWITTER_HANDLE = 'yappi_web3';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
 
 const App = () => {
+  const [currentAccount, setCurrentAccount] = useState("");
+  console.log("currentAccount: ", currentAccount);
+
+  const checkConnectedChain = async () => {
+    let chainId = await ethereum.request({ method: "eth_chainId"});
+    console.log("Connected to chain" + chainId);
+    const goerliChainId = "0x5";
+    if (chainId !== goerliChainId) {
+      alert("You are not connected to the Goerli Test Network!");
+    }
+  };
+
+  /*
+  * validate to MetaMask installed
+  */
+ const checkIfWallerIsConnected = async () => {
+    const { ethereum } = window;
+    if (!ethereum) {
+      console.log("Make sure you have MetaMask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+
+    if(accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account);  
+
+      setupEventListener();
+    } else {
+      console.log("No authorized account found");
+    }  
+  };
+
+ const connectWallet = async () => {
+  try {
+    const { ethereum } = window;
+    if (!ethereum) {
+      alert("Get MetaMask!");
+      return;
+    }
+
+    const accounts = await ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    console.log("Connected", accounts[0]);
+    setCurrentAccount(accounts[0]);
+
+    setupEventListener();
+  } catch (error) {
+    console.log(error);
+  }
+ };
+
+ const setupEventListener = async () => {
+  try {
+    const { ethereum } = window;
+    if (ethereum) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const connectedContract = new ethers.Contract(
+        CONTRACT_ADDRESS,
+        myEpicNft.abi,
+        signer
+      );
+      connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+        console.log(from, tokenId.toNumber());
+        alert(
+          `あなたのウォレットに NFT を送信しました。OpenSea に表示されるまで最大で10分かかることがあります。NFT へのリンクはこちらです: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`
+        );
+      });
+      console.log("Setup event listener!");
+    } else {
+      console.log("Etherum object doesn't exist!");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+ };
+
+ const askContractToMintNft = async () => {
+    const CONTRACT_ADDRESS = "0xdec95048BB9413475228097cb19CF0B20f284bA9";
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          myEpicNft.abi,
+          signer
+        );
+
+        console.log("Going to pop wallet now to pay gas...");
+        let nftTxn = await connectedContract.makeAnEpicNFT();
+        console.log("Mining...please wait.");
+        await nftTxn.wait();
+  
+        console.log(
+          `Mined, see transaction: https://goerli.etherscan.io/tx/${nftTxn.hash}`
+        );
+    
+      } else {
+        console.log("Ethereum object dosen't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+ };
+
   // renderNotConnectedContainer メソッドを定義します。
   const renderNotConnectedContainer = () => (
-    <button className="cta-button connect-wallet-button">
+    <button 
+    onClick={connectWallet}
+    className="cta-button connect-wallet-button"
+    >
       Connect to Wallet
     </button>
   );
+
+  useEffect(() => {
+    checkConnectedChain();
+    checkIfWallerIsConnected();
+  }, []);
 
   return (
     <div className="App">
@@ -24,7 +147,13 @@ const App = () => {
           <p className="sub-text">
             あなただけの特別な NFT を Mint しよう💫
           </p>
-          {renderNotConnectedContainer()}
+          {currentAccount === "" ? (
+            renderNotConnectedContainer()
+          ) : (
+            <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
+              Mint NFT
+            </button>
+          )}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
